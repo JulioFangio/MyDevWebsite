36789bf1c1e3265fa0e6ced98b079324db4e607e
JulioFangio <duvalgirardjules@gmail.com>
2025-11-03 14:31:55 +0100

    Fix: Configuration Netlify pour API routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-// import netlify from '@astrojs/netlify';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
-  // output: 'server',
-  // adapter: netlify(),
+  output: 'server',
+  adapter: netlify(),
   integrations: [tailwind()],
 });
